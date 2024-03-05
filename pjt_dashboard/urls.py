36195186c8c92aa0808efa1890970e7c7ad6fde5
Leonardo <leonardo@contabilgaulke.com.br>
2024-03-05 15:56:15 -0300

    feat: remoção da route /v3/ da url base.

--- pjt_dashboard/urls.py
+++ pjt_dashboard/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('web/v3/', include("app_dashboard.urls")),
+    path('web/', include("app_dashboard.urls")),
 ]
